9bc10c0aa1e3f5c6ae8d7fe7f306c116e2727ac8
Mateus Coutinho <maateus.coutinho@gmail.com>
2025-12-26 10:02:36 -0300

    prevents script execution as root

--- main.sh
+++ main.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+if [[ $EUID -eq 0 ]]; then
+   echo "This script must not be run as root"
+   exit 1
+fi
+
 source ./repositories/rpmfusion.sh
 source ./repositories/flathub.sh
 
