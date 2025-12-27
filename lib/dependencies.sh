2f8c2207a3dcad1cee4dcbdb2aed97a6ab3dd66a
Mateus Coutinho <maateus.coutinho@gmail.com>
2025-12-26 22:06:30 -0300

    adds desktop environment selection menu

--- lib/dependencies.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-sudo dnf install -y \
-  pciutils
--- main.sh
+++ main.sh
@@ -2,16 +2,26 @@
 set -euxo pipefail
 
 if [[ $EUID -eq 0 ]]; then
-   echo "This script must not be run as root"
-   exit 1
+  echo "This script must not be run as root"
+  exit 1
 fi
+
+# Dependencies
+sudo dnf install -y newt pciutils
+
+DESKTOP_ENVIRONMENT=$(whiptail --title "My Fedora" --notags \
+  --menu "Select you preferred desktop environment:" 15 60 4 \
+  "gnome" "GNOME Desktop (recommended)" \
+  "plasma" "KDE Plasma Desktop" \
+  3>&1 1>&2 2>&3)
 
 source ./repositories/rpmfusion.sh
 source ./repositories/flathub.sh
 
-source ./lib/dependencies.sh
-
-source ./desktop/gnome.sh
+case "$DESKTOP_ENVIRONMENT" in
+"gnome") source ./desktop/gnome.sh ;;
+"plasma") source ./desktop/plasma.sh ;;
+esac
 
 source ./desktop/postinstall.sh
 
@@ -20,4 +30,4 @@ source ./internet/firefox.sh
 source ./development/docker.sh
 source ./development/vscode.sh
 
-source ./games/steam.sh
+source ./gaming/steam.sh
