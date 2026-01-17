8f83f5fbf5d28bfc979e3d51a8d6ca3f8dbbea17
Mateus Coutinho <maateus.coutinho@gmail.com>
2026-01-17 14:45:59 -0300

    adds usage types selection menu

--- desktop/postinstall.sh
+++ desktop/postinstall.sh
@@ -2,11 +2,11 @@
 
 # If it's a laptop, use TLP for power management; otherwise, use tuned for performance.
 if grep -q "Battery" /sys/class/power_supply/*/type; then
-    sudo dnf install -y tlp tlp-rdw
-    sudo dnf remove -y power-profiles-daemon tuned tuned-ppd
-    sudo systemctl enable --now tlp.service
-    sudo systemctl mask systemd-rfkill.service systemd-rfkill.socket
+  sudo dnf install -y tlp tlp-rdw
+  sudo dnf remove -y power-profiles-daemon tuned tuned-ppd
+  sudo systemctl enable --now tlp.service
+  sudo systemctl mask systemd-rfkill.service systemd-rfkill.socket
 else
-    sudo systemctl start tuned tuned-ppd
-    sudo tuned-adm profile latency-performance
+  sudo systemctl start tuned tuned-ppd
+  sudo tuned-adm profile latency-performance
 fi
--- main.sh
+++ main.sh
@@ -6,18 +6,29 @@ if [[ $EUID -eq 0 ]]; then
   exit 1
 fi
 
-# Dependencies
-sudo dnf install -y curl tar newt pciutils
+# User Preferences
+sudo dnf install -y newt
 
 DESKTOP_ENVIRONMENT=$(whiptail --title "My Fedora" --notags \
-  --menu "Select you preferred desktop environment:" 15 60 4 \
+  --menu "Select your preferred desktop environment:" 15 60 4 \
   "gnome" "GNOME Desktop (recommended)" \
   "plasma" "KDE Plasma Desktop" \
   3>&1 1>&2 2>&3)
 
+USAGE_TYPES=$(whiptail --title "My Fedora" --notags \
+  --checklist "Select your intended use: (SPACE to select)" 15 60 4 \
+  "development" "Development" ON \
+  "gaming" "Gaming" OFF \
+  3>&1 1>&2 2>&3)
+
+# Repositories
 source ./repositories/rpmfusion.sh
 source ./repositories/flathub.sh
 
+# Dependencies
+sudo dnf install -y curl tar pciutils
+
+# Desktop Environment
 case "$DESKTOP_ENVIRONMENT" in
 "gnome") source ./desktop/gnome.sh ;;
 "plasma") source ./desktop/plasma.sh ;;
@@ -25,10 +36,17 @@ esac
 
 source ./desktop/postinstall.sh
 
+# Applications
 source ./internet/firefox.sh
 
-source ./development/docker.sh
-source ./development/jetbrains-toolbox.sh
-source ./development/vscode.sh
+# Development
+if [[ $USAGE_TYPES == *"development"* ]]; then
+  source ./development/docker.sh
+  source ./development/jetbrains-toolbox.sh
+  source ./development/vscode.sh
+fi
 
-source ./gaming/steam.sh
+# Gaming
+if [[ $USAGE_TYPES == *"gaming"* ]]; then
+  source ./gaming/steam.sh
+fi
