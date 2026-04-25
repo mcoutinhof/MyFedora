f2bdc6ce1acececadd913a76951b172c3c898478
Mateus Coutinho <maateus.coutinho@gmail.com>
2026-04-25 16:53:34 -0300

    adds default cinnamon theme for lightdm

--- desktop/cinnamon.sh
+++ desktop/cinnamon.sh
@@ -2,23 +2,23 @@
 
 (
   declare -r PACKAGES=(
-    cinnamon                # Base desktop environment
-    lightdm                 # Login manager
+    cinnamon                       # Base desktop environment
+    lightdm slick-greeter-cinnamon # Login manager
 
-    gnome-terminal          # Terminal emulator
-    nemo                    # File manager
-    file-roller             # Archive manager
-    gnome-system-monitor    # System monitor
-    gnome-disk-utility      # Partition manager
-    gnome-logs              # Log viewer
-    baobab                  # Disk usage analyzer
+    gnome-terminal                 # Terminal emulator
+    nemo                           # File manager
+    file-roller                    # Archive manager
+    gnome-system-monitor           # System monitor
+    gnome-disk-utility             # Partition manager
+    gnome-logs                     # Log viewer
+    baobab                         # Disk usage analyzer
 
-    gnome-calculator        # Calculator
-    xed                     # Text editor
-    xreader                 # Document viewer
-    eom                     # Image viewer
-    rhythmbox               # Audio player
-    celluloid               # Video player
+    gnome-calculator               # Calculator
+    xed                            # Text editor
+    xreader                        # Document viewer
+    eom                            # Image viewer
+    rhythmbox                      # Audio player
+    celluloid                      # Video player
   )
 
   sudo dnf install -y "${PACKAGES[@]}"
